f79b175fa1acb4b4df12fab33bef225de61719b7
Chinghua Chuan <chinghua@gmail.com>
2018-01-16 11:03:24 -0500

    basic mobile app
    The basic mobile app in Unity including navigation and content panel

---
+++ Assets/Scripts/InstructionModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InstructionModel{
+	private List<InstructionStep> steps = new List<InstructionStep>();
+
+	public void LoadData() {
+		steps.Clear ();
+		TextAsset text_asset = (TextAsset)Resources.Load ("instructionsCSV");
+		fgCSVReader.LoadFromString (text_asset.text, new fgCSVReader.ReadLineDelegate(csvReader));
+
+		/*
+		steps.Add (new InstructionStep(new List<string>{"0", "1st Step", "Details in the 1st Step."}));
+		steps.Add (new InstructionStep(new List<string>{"1", "2nd Step", "Details in the 2nd Step."}));
+		steps.Add (new InstructionStep(new List<string>{"2", "3rd Step", "Details in the 3rd Step."}));
+		*/
+	}
+
+	// public getter method: to get a specific step
+	public InstructionStep GetInstructionStep(int index) {
+		if (index < 0 || index >= steps.Count)
+			return null;
+		return steps [index];
+	}
+
+	public int GetCount() {
+		return steps.Count;
+	}
+
+	// helper method required by fgCSVReader
+	private void csvReader(int line_index, List<string> line) {
+		if (line_index == 0) 
+			return;
+		steps.Add (new InstructionStep (line));
+	}
+}
---
+++ Assets/Scripts/InstructionsController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionsController : MonoBehaviour {
+
+	public Text stepText;
+	public Text titleText;
+	public Text bodyText;
+
+	private int currentStep;
+	private InstructionModel currentInstructionModel = new InstructionModel ();
+
+	// Use this for initialization
+	void Awake () {
+		currentInstructionModel.LoadData ();
+	}
+
+	void Start () {
+		currentStep = 0;
+		CurrentInstructionUpdate ();
+	}
+
+	public void NextStep() {
+		if (currentStep < currentInstructionModel.GetCount()-1) {
+			currentStep++;
+			CurrentInstructionUpdate ();
+		}
+	}
+
+	public void PreviousStep() {
+		if (currentStep > 0) {
+			currentStep--;
+			CurrentInstructionUpdate ();
+		}
+	}
+
+	private void CurrentInstructionUpdate() {
+		InstructionStep step = currentInstructionModel.GetInstructionStep (currentStep);
+		stepText.text = "Step: " + currentStep;	
+		titleText.text = step.Title;
+		bodyText.text = step.BodyText;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
